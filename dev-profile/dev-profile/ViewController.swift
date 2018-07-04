ee0a950d7cadda4b315af47f990caf9e30cfda39
МакЛарен <maklaren@MacBook-Air-MakLaren.local>
2018-07-04 18:47:19 +0600

    init xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by МакЛарен on 04.07.2018.
+//  Copyright © 2018 Asset Ryskul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
